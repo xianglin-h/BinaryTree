6539afaa538d9ba4a564133b7d74d87672324289
xianglin-h <xianglin_sunny@163.com>
2020-12-16 07:43:54 +0800

    增加功能

--- binarytree.cpp
+++ binarytree.cpp
@@ -326,3 +326,127 @@ int CalcTwoNodeDistance(Node *node, Node *target1, Node *target2){
 }
 
 /*11.找出二叉树中某个节点的所有祖先节点*/
+					
+					1
+				   / \
+			      2   7
+				 / \   \
+				3   4   8 
+				   / \   \
+				  5   6   9
+				         / \
+						10  11
+/*如果给定结点 5，则其所有祖先结点为 4,2,1*/
+bool FindNodeAllLCA(Node *node, Node *target){
+	if(nullptr == node){
+		return false;
+	}
+	if(node == target){
+		return true;
+	}
+	
+	if(FindNodeAllLCA(node->left, target) || FindNodeAllLCA(node->right, target)){
+		cout << node->data << ' ';
+		return true;
+	}
+	
+	return false;
+}					
+
+/*13.二叉树前序中序推后序*/
+/*
+方式	序列
+前序	[1 2 4 7 3 5 8 9 6]
+中序	[4 7 2 1 8 5 9 3 6]
+后序	[7 4 2 8 9 5 6 3 1]
+
+根据上述可以推出二叉树的图形
+				1
+			   / \
+			  2   3
+			 /   / \
+			4   5   6
+			 \ / \
+			  78  9
+			  
+以上面图表为例，步骤如下：
+
+根据前序可知根结点为1；
+根据中序可知 4 7 2 为根结点 1 的左子树和 8 5 9 3 6 为根结点 1 的右子树；
+递归实现，把 4 7 2 当做新的一棵树和 8 5 9 3 6 也当做新的一棵树；
+在递归的过程中输出后序。
+*/
+
+
+/*14.判断二叉树是不是完全二叉树
+若设二叉树的深度为 h，除第 h 层外，其它各层 (1～h-1) 的结点数都达到最大个数，第 h 层所有的结点都连续集中在最左边，这就是完全二叉树（Complete Binary Tree）。如下图：图片
+
+					  1
+				/		   \
+			   2		   3
+			 /	 \        /   \
+           4       5      6      7
+          / \     / \    /
+         8   9   10  11 12		  
+首先若一个结点只有右孩子，肯定不是完全二叉树；其次若只有左孩子或没有孩子，那么接下来的所有结点肯定都没有孩子，否则就不是完全二叉树，因此设置 flag 标记变量。
+*/
+bool IsCBT(Node *node){
+	
+	bool flag = false;
+	queue<Node *> Q;
+	Q.push(node);
+	
+	while(!Q.empty()){
+		
+		Node *p = Q.front();
+		Q.pop();
+		if(flag){
+			if(p->left || p->right)
+			return false;
+		}
+		else{
+			
+			if(p->left && p->right){
+				
+				Q.push(p->left);
+				Q.push(p->right);
+			}else if(p->left){
+				
+				return true;
+			}else if(p->right){
+				
+				return false;
+			}else{
+					//没有节点
+				return true;
+			}
+		}
+	}
+	return true;
+}
+
+/*15判断是否是二叉树的后序遍历结果
+在后续遍历得到的序列中，最后一个元素为树的根结点。从头开始扫描这个序列，比根结点小的元素都应该位于序列的左半部分；从第一个大于跟结点开始到跟结点前面的一个元素为止，所有元素都应该大于跟结点，因为这部分元素对应的是树的右子树。根据这样的划分，把序列划分为左右两部分，我们递归地确认序列的左、右两部分是不是都是二元查找树。*/
+int array[n];	/*长度为n的序列，注意begin和end遵循的是左闭右闭原则*/
+bool IsSequenceOfBST(int begin, int iEnd){
+	
+	if(iEnd - begin <= 0){
+		return true;
+	}
+	
+	int iRootData = array[iEnd];	//数组尾为根节点
+	int i = begin;
+		
+	for(; array[i] < iRootData; i++)
+		;
+	
+	int j = i;
+	
+	for(; j < iEnd; j++){
+		if(array[j] < iRootData){
+			return false;
+		}
+	}
+	
+	return IsSequenceOfBST(begin, i - 1) && IsSequenceOfBST(i, iEnd - 1);
+}
